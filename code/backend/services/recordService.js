eff0e33902ee7171be2d14ebd21f2b2fbcd3ed9f
SHUHEI ARAI <sharai.hm@kaien-t.net>
2024-07-10 20:03:47 +0900

    Remove unnecessary files

--- code/backend/services/recordService.js
+++ code/backend/services/recordService.js
@@ -2,42 +2,13 @@
 const mongoose = require('mongoose');
 
 // 各ページのモデルをインポート
-const Page0Model = require('../models/p0Cover'); // 他のページモデルも同様にインポート
-const Page1Model = require('../models/p1householdRegistry');
-const Page2Model = require('../models/p2maternalHealth');
-const Page3Model = require('../models/p3maternalLivingEnvironment');
-const Page4Model = require('../models/p4pregnancyRecord3_4');
-const Page5Model = require('../models/p5pregnancyRecord5_6');
-const Page6Model = require('../models/p6PregnancyRecord7_8');
-const Page7Model = require('../models/p7PregnancyRecord9_10');
-const Page8Model = require('../models/p8pregnancyProgress');
-const Page10Model = require('../models/p10ExaminationRecord');
-const Page11Model = require('../models/p11reserveField');
-const Page12Model = require('../models/p12MaternalClassRecord');
-const Page13Model = require('../models/p13DentalStatus');
-const Page14Model = require('../models/p14ChildbirthStatus');
-const Page15Model = require('../models/p15PostpartumStatus');
-const Page16Model = require('../models/p16NeonatalProgress');
-const Page17Model = require('../models/p17ChildExaminationRecord');
-const Page18Model = require('../models/p18ParentRecord1');
-const Page21Model = require('../models/p21OneMonthHealthCheck1');
-const {Page22Model , P22Schema }= require('../models/p22ParentRecordThreeToFourMonths');
-const {Page23Model , P23Schema }= require('../models/p23ThreeToFourMonthsHealthCheck');
+const {Page22Model}= require('../models/p22ParentRecordThreeToFourMonths');
+const {Page23Model}= require('../models/p23ThreeToFourMonthsHealthCheck');
 
 // ...他のページのモデルもインポート
 
 // 共通のCRUD操作関数
 
- // 利用するスキーマを基にバリデーションや整形を行う関数(対象データ、対象モデル、対象スキーマ)
- const validateAndFormatData = (data,schema) => {
-  const Model = mongoose.model('TempModel', schema);
-  const document = new Model(data);
-  const error = document.validateSync();
-  if (error) {
-  throw new Error(error.message);
-   }
-  return document.toObject();
-  };
 
 // レコードの作成
 const createRecord = async (model, data) => {
